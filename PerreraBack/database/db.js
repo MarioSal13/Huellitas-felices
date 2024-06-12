74fc551a5d6f8f28f3c6e727f0927d820a219974
MarioSal13 <mariosalmartinezren@gmail.com>
2024-06-12 14:01:14 -0700

    conxion a la DB hecha?

--- PerreraBack/database/db.js
+++ PerreraBack/database/db.js
@@ -1,9 +1,16 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('perreradb','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
+// Configuración directa de la base de datos (no recomendado para producción)
+const db = new Sequelize(
+    'perreradb',  // Nombre de la base de datos
+    'root',       // Usuario de la base de datos
+    'zqFkWVWvHmxEHdbemuzEfKCANDPAWCEn', // Contraseña del usuario de la base de datos
+    {
+        host: 'monorail.proxy.rlwy.net', // Host de la base de datos
+        dialect: 'mysql',
+        dialectModule: require('mysql2'),
+        logging: false, // Puedes habilitar el logging si lo necesitas
+    }
+);
 
-})
-
-export default db
+export default db;
